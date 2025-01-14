94a267cea457885074626f4db784e2a01dd4de72
Maxim Kabalinov <m.kabalinov@itpelag.com>
2025-01-14 21:06:33 +0300

    Начальная настройка проекта

---
+++ resources/js/pages/test.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Test() {
+  return (
+    <div className="m-8">
+      <h1 className="text-3xl">Тестовая сраница</h1>
+      <p className="text-md">Дизайн плоховат</p>
+      <Button variant="outline" className="mt-4">Очень некрасивая кнопка</Button>
+    </div>
+  );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('test');
 });
