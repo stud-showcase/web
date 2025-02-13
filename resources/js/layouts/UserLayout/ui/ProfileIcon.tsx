79d2186025ca477e23a33feb720e1c76eed27380
Maxim Kabalinov <maximdipll8@gmail.com>
2025-02-13 23:38:26 +0300

    Добавил темную тему

--- resources/js/layouts/UserLayout/ui/ProfileIcon.tsx
+++ resources/js/layouts/UserLayout/ui/ProfileIcon.tsx
@@ -4,16 +4,33 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/shared/ui/DropdownMenu";
-import { LogOut, Settings, User } from "lucide-react";
+import {
+  LogOut,
+  Mail,
+  MessageSquare,
+  Moon,
+  PlusCircle,
+  Settings,
+  Sun,
+  User,
+  UserPlus,
+} from "lucide-react";
 import { useContext } from "react";
 import { AuthContext } from "../state";
 import { Link } from "@inertiajs/react";
+import { useTheme } from "@/shared/providers/ThemeProvider";
+import { Button } from "@/shared/ui/Button";
 
 export function ProfileIcon() {
   const { setIsLoggedIn } = useContext(AuthContext);
+  const { setTheme } = useTheme();
 
   const logout = () => {
     localStorage.removeItem("isLoggedIn");
@@ -21,40 +38,64 @@ export function ProfileIcon() {
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <div className="flex cursor-pointer items-center gap-2">
-          <Avatar>
-            <AvatarFallback className="bg-primary text-white">
-              ИИ
-            </AvatarFallback>
-          </Avatar>
-          <span className="font-semibold text-sm text-primary">
-            Иван Иванов
-          </span>
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Иванов И.И.</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Link href="/profile">
-            <User className="mr-2 h-4 w-4" />
-            <span>Профиль</span>
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/admin/dashboard">
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Перейти в админ-панель</span>
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={logout}>
-          <LogOut className="mr-2 h-4 w-4 text-destructive" />
-          <span className="text-destructive">Выйти из аккаунта</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex cursor-pointer items-center gap-2">
+            <Avatar>
+              <AvatarFallback className="bg-primary text-white">
+                ИИ
+              </AvatarFallback>
+            </Avatar>
+            <span className="font-semibold text-sm text-primary">
+              Иван Иванов
+            </span>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel>Иванов И.И.</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Link href="/profile">
+              <User className="mr-2 h-4 w-4" />
+              <span>Профиль</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/admin/dashboard">
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Админ-панель</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Sun className="mr-2 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="mr-2 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span>Тема</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  <span>Светлая</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  <span>Темная</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  <span>Системная</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={logout}>
+            <LogOut className="mr-2 h-4 w-4 text-destructive" />
+            <span className="text-destructive">Выйти</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
