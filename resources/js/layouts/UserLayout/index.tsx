3a52a003a9fb9c9e1352c008339ac5ed066abdcc
Maxim Kabalinov <maximdipll8@gmail.com>
2025-03-04 21:38:53 +0300

    Убрал темную тему (если что можно будет легко вернуть её обратно)

--- resources/js/layouts/UserLayout/index.tsx
+++ resources/js/layouts/UserLayout/index.tsx
@@ -2,16 +2,15 @@ import { ReactNode } from "react";
 import { Header } from "./ui/Header";
 import { Footer } from "./ui/Footer";
 import { Container } from "@/shared/ui/Container";
-import { ThemeProvider } from "@/shared/providers/ThemeProvider";
 
 export function UserLayout({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="ui-theme">
+    <>
       <Container className="h-screen px-6 py-8">
         <Header />
         <main className="pt-9">{children}</main>
       </Container>
       <Footer />
-    </ThemeProvider>
+    </>
   );
 }
--- resources/js/layouts/UserLayout/ui/ProfileIcon.tsx
+++ resources/js/layouts/UserLayout/ui/ProfileIcon.tsx
@@ -4,32 +4,20 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/shared/ui/DropdownMenu";
 import {
   LogOut,
-  Mail,
-  MessageSquare,
-  Moon,
-  PlusCircle,
   Settings,
-  Sun,
   User,
-  UserPlus,
 } from "lucide-react";
 import { useContext } from "react";
 import { AuthContext } from "../state";
 import { Link } from "@inertiajs/react";
-import { useTheme } from "@/shared/providers/ThemeProvider";
 
 export function ProfileIcon() {
   const { setIsLoggedIn } = useContext(AuthContext);
-  const { setTheme } = useTheme();
 
   const logout = () => {
     localStorage.removeItem("isLoggedIn");
@@ -63,31 +51,9 @@ export function ProfileIcon() {
           <DropdownMenuItem asChild>
             <Link href="/admin/dashboard">
               <Settings className="mr-2 h-4 w-4" />
-              <span>Админ-панель</span>
+              <span>Панель администратора</span>
             </Link>
           </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>
-              <Sun className="mr-2 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="mr-2 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span>Тема</span>
-            </DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                  <span>Светлая</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                  <span>Темная</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                  <span>Системная</span>
-                </DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={logout}>
             <LogOut className="mr-2 h-4 w-4 text-destructive" />
