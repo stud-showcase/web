857ababa80db63a59d86792f539e9f0909d5c7c0
Maxim Kabalinov <maximdipll8@gmail.com>
2025-03-08 18:06:10 +0300

    Поправил tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,6 @@ export default {
     "./resources/js/**/*.tsx",
   ],
   safelist: [
-    'font-myriad',
-    'font-minion',
-    "text-sm",
-    "text-md",
-    "text-lg",
-    "text-xl",
-    "text-2xl",
-    "text-3xl",
-    "text-4xl",
     "fill-primary",
     "fill-white"
   ],
