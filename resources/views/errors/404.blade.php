3e13d48bda55bfad03abeeba8b026e4cece33777
Maxim Kabalinov <maximdipll8@gmail.com>
2025-04-20 18:24:51 +0300

    Подправил название страницы 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Ошибка 500</title>
+        <title>Ошибка 404</title>
         @vite('resources/css/app.css')
     </head>
     <body
