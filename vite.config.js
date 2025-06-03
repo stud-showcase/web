974f1502d08d2c69b59a42e939cae87170718c16
Suren Arakelyan <s.arakelian@bnovo.ru>
2025-06-03 22:44:04 +0300

    Поменял MySQL на PostgreSQL

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true,
     hmr: {
       host: 'localhost',
-      port: 5189,
     },
   },
 });
